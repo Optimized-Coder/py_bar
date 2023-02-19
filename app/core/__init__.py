3e708cf427bfdd42b28b19075c4ea143feb37a16
[Josh Shepherd] <[optimizedcoder.py@gmail.com]>
2023-02-19 21:18:53 +0000

    EOD cleanup

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'blank'
+    app.config['SECRET_KEY'] = os.environ.get("secret_key")
     app.config['FLASK_ENV'] = 'development'
     
-
     # define BP's
     from .main import bp as main_bp
     app.register_blueprint(main_bp)
